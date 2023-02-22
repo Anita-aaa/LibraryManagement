f35cf6b449b572ae6397ccad69108c66eebf5e1d
UPESHRATALA <upesratala14@gmail.com>
2023-02-23 00:11:17 +0545

    admin_pannel

--- admin.py
+++ admin.py
@@ -14,3 +14,17 @@ def loginlibr():
         window.withdraw()
         closedb()
         home()
+def libr():
+    global window
+    window.withdraw()
+    global win,b1,b2,b3,b4
+    win=Tk()
+    win.title('Library')
+    win.geometry("400x400+480+180")
+    win.resizable(False,False)
+    win.mainloop()
+
+def loginadmin():
+    if e1.get()=='admin' and e2.get()=='admin':
+        admin();
+        
