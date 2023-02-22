6a5878b618dcd2ed588fb922a4f6bd132a0422c9
UPESHRATALA <upesratala14@gmail.com>
2023-02-23 00:09:57 +0545

    connectdb

--- admin.py
+++ admin.py
@@ -1,3 +1,16 @@
 from tkinter import *
+import pymysql as p
 
-
+def loginlibr():
+    global window
+    connectdb()
+    for i in range(cur.rowcount):
+        data=cur.fetchone()
+        if e1.get().strip()==str(data[1]) and e2.get().strip()==str(data[3]):
+            closedb()
+            libr()
+            break
+    else:
+        window.withdraw()
+        closedb()
+        home()
